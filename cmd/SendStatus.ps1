b68eff20708aa3730bfeb5b370025716ce64b7fd
Your Name <you@example.com>
2021-09-25 17:14:42 -0400

    status

---
+++ cmd/SendStatus.ps1
@@ -0,0 +1,22 @@
+[CmdletBinding(SupportsShouldProcess=$true)]
+param (
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$subject,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$msgbody
+    )
+    $email = "radicaltronic@gmail.com"
+    $recipients = "radicaltronic@gmail.com,guillaumeplante.qc@gmail.com"
+    $pass = "SecretTEst23_"
+
+    $EmailFrom = "radicaltronic@gmail.com"
+    $EmailTo = "guillaumeplante.qc@gmail.com"
+    $Subject = $subject
+    $Body = $msgbody
+    $SMTPServer = "smtp.gmail.com"
+    $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+    $SMTPClient.EnableSsl = $true
+    $SMTPClient.Credentials = New-Object System.Net.NetworkCredential($email, $pass);
+    $SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
