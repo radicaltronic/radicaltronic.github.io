2922d6148a3de5d1241b15b6daa31f93db1d7576
Your Name <you@example.com>
2021-09-26 01:25:52 -0400

    'latest'

--- cmd/reboot.ps1
+++ cmd/reboot.ps1
@@ -1,20 +1,43 @@
 
-Function Send-Notification
-{
-    param (
-        [Parameter(Mandatory)]
+function Write-Log {
+    [CmdletBinding()]
+    param(
+        [Parameter()]
         [ValidateNotNullOrEmpty()]
-        [string]$subject,
-        [string]$msgbody
+        [string]$Message,
+ 
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet('Information','Warning','Error')]
+        [string]$Severity = 'Information'
     )
+   
+    [pscustomobject]@{
+        Time = (Get-Date -f g)
+        Message = $Message
+        Severity = $Severity
+    } | Export-Csv -Path "$env:Temp\LogFile.csv" -Append -NoTypeInformation
+}
+
+
+Function Send-RestartNotification
+{
+    $CurDatePretty=Get-Date
+
+    Write-Log -Message "-------------- COMPUTER RESTART NOTIFICATION -------------- "
+    Write-Log -Message "$CurDatePretty"
+    Write-Log -Message "Logged in as $env:username on local host $env:computername"
+
+    $Logs=Get-Content -Path "$env:Temp\LogFile.csv" -Raw
+    $Subject='COMPUTER RESTART NOTIFICATION FROM $env:COMPUTERNAME'
+    $Body=$Logs
+
     $email = "radicaltronic@gmail.com"
-    $recipients = "radicaltronic@gmail.com"
+    $recipients = "radicaltronic@gmail.com,guillaumeplante.qc@gmail.com"
     $pass = "SecretTEst23_"
 
     $EmailFrom = "radicaltronic@gmail.com"
-    $EmailTo = "guillaumeplante.qc@gmail.com"
-    $Subject = $subject
-    $Body = $msgbody
+    $EmailTo = "radicaltronic@gmail.com,guillaumeplante.qc@gmail.com"
     $SMTPServer = "smtp.gmail.com"
     $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
     $SMTPClient.EnableSsl = $true
@@ -22,5 +45,6 @@ Function Send-Notification
     $SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
 }
 
-Send-Notification "restarting pc" "restarting in 30 seconds"
-Restart-Computer -Timeout 30 -Force
+
+Send-RestartNotification 
+Restart-Computer -WhatIf
